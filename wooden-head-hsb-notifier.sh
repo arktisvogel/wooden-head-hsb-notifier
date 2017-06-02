0da8205cfeec9a5d418145d2050a069acf347563
arktisvogel <cpf@online.ms>
2017-06-02 10:29:41 +0200

    Decided to implement jshon without any parameters.

--- wooden-head-hsb-notifier.sh
+++ wooden-head-hsb-notifier.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
-# This shell script invokes ``curl''   
+# This shell script invokes ``curl''
 # in order to query the Hackerspace Bremen database.
-# It processes the JSON stream with the 
+# It processes the JSON stream with the
 # program ``jshon'' (Copyright 2011 Kyle Keen <keenerd@gmail.com>).
-# Finally, it invokes AWK to only write those lines which
-# contain the string:
-#   "open"
+#
 # Copyright ©2016 Craig Parker-Feldmann.
 # Licensed under the GNU General Public License, Version 3
 #
@@ -13,8 +11,15 @@ STAMM=$(basename $0)
 HSB_URL=https://hackerspacehb.appspot.com/v2/status
 
 echo 'Shell script "'$0'" now running…' >&2
+
 curl --silent $HSB_URL | \
-    jshon -s "open" | \
+  jshon | \
+  tr '\}' '\n' | \
+  grep '\"ST3\"'
+
 echo 'Shell script "'$0' done.' >&2
+#
+# As of 2017-06-02, the output of this shell script looks like this:
+#
 #
 # EOF
