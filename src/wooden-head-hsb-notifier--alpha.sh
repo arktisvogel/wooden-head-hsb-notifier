82f2c807a13448a8d7a2d90819df0cc921bd821f
arktisvogel <cpf@online.ms>
2017-06-17 17:30:22 +0200

    Just deleted a file.

--- src/wooden-head-hsb-notifier--alpha.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/lua
--- This shell script executes a system call (executes curl, piped to jshon), which will fetch a JSON stream from the Hackerspace Bremen website.
---
-os.execute "curl  -s https://hackerspacehb.appspot.com/v2/status | jshon"
---
--- EOF
