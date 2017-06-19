d997c0f49f1aa60241cff1dd01d317150a5ef609
arktisvogel <lipsticky@magic.ms>
2017-06-19 13:59:34 +0200

    Shell script as lua shell script.
    The shell script invokes lua, which executes a call to the operating system. That call invokes curl and jshon.

--- src/wooden-head-hsb-notifier.sh
+++ src/wooden-head-hsb-notifier.sh
@@ -1,36 +1,6 @@
-#! /bin/bash
-# This shell script invokes ``curl''
-# in order to query the Hackerspace Bremen database.
-# It processes the JSON stream with the
-# program ``jshon'' (Copyright 2011 Kyle Keen <keenerd@gmail.com>).
-#
-# Copyright ©2016 Craig Parker-Feldmann.
-# Licensed under the GNU General Public License, Version 3
-#
-STAMM=$(basename $0)
-HSB_URL=https://hackerspacehb.appspot.com/v2/status
-
-echo 'Shell script "'$0'" now running…' >&2
-
-curl --silent $HSB_URL | \
-  jshon | \
-  tr '\}' '\n' | \
-  grep '\"ST3\"'
-# The above invocation is equivalent to:
-#   curl -s https://hackerspacehb.appspot.com/v2/status | jshon | tr '\}' '\n' | grep '\"ST3\"'
-
-echo 'Shell script "'$0' done.' >&2
-#
-# As of 2017-06-14, the output of this shell script looks like this:
-#
-# In the morning:
-#   Shell script "./wooden-head-hsb-notifier.sh" now running…
-#     "ST3": "CLOSED",
-#   Shell script "./wooden-head-hsb-notifier.sh done.
-#
-# In the afternoon:
-#   Shell script "./wooden-head-hsb-notifier.sh" now running…
-#     "ST3": "OPEN",
-#   Shell script "./wooden-head-hsb-notifier.sh done.
-#
-# EOF
+#! /usr/bin/lua
+-- This shell script executes a system call (executes curl, piped to jshon), which will fetch a JSON stream from the Hackerspace Bremen website.
+--
+os.execute "curl  -s https://hackerspacehb.appspot.com/v2/status | jshon"
+--
+-- EOF
